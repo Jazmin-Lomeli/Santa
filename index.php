3b900eaa1e7fc57344ab4b09d97b4b4d4e33267e
Aarón Yael Carranza Vera <70918693+ANYL2000@users.noreply.github.com>
2023-03-13 22:45:15 -0600

    Cambio de interfaz

--- index.php
+++ index.php
@@ -1,110 +1,87 @@
-<?php
-$procesos = 0;
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-
-
-  $procesos = $procesos + $_POST["new"];
-
-}
-
-?>
 <!DOCTYPE html>
-<html lang="en">
+<html>
+  <head>
+    <title>Santa Claus</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+  </head>
+  <body>
 
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Santa</title>
+ 
 
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-    integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+  <div class="card" style="box-shadow: 0px 0px 10px gray; background-image: url('https://i.gifer.com/7t5M.gif');"> 
+    <div class="card-body">
+    <h1 class="card-title text-center font-weight-bold" style="color: white;">Santa Claus</h1>
+    </div>
+    </div>
 
-</head>
+    <br>
 
-<body>
+    <div class="container" style="ma-width: 100%">
 
-  <div class="container mt-3 mb-2">
-    <div class="row">
-      <div class="mx-auto">
-        <h2>
-          Santa
-        </h2>
-      </div>
+ 
 
+      <div class="row">
 
-      <div class="container mt-4 mx-0 border rounded shadow">
-        <div class="row justifi-conten-center align-items-center pt-3">
+ 
+        <div class="col-sm-12 col-md-4">
+        <div class="card" style="box-shadow: 0px 0px 10px gray"> 
+            <div class="card-body">
 
-          <div class="col-lg-4 form-group text-center pb-2">
+            <div class="row">
+                <div class="col-md-8">
+          <h2 class="card-title">Renos</h2>
+                </div>
 
-            <form method="post" id="formulario">
-              <h4> <label for="new">Agregar procesos</label></h4>
+                <div class="col-md-4">
+          <h2  class="font-weight-bold">0</h2>
+                </div>
+            </div>
 
-              <input type="number" id="new" name="new" value="<?php echo $procesos ?>" min="<?php echo $procesos ?>"
-                max="200">
+          <img src="https://i.pinimg.com/originals/88/bb/97/88bb97748362baa4c1b116b3ee1a589d.gif" class="img-fluid" alt="Renos Navideños">
+          <button type="button" class="btn btn-primary btn-block">Agregar proceso de Reno</button>
+          </div>
+          </div>
+            <br>
+            <div class="card" style="box-shadow: 0px 0px 10px gray">
+            <div class="card-body">
+         
 
-              <abbr title="Agregar">
-                <button type="submit" class="px-2 btn btn-success"><i class="bi bi-plus-square"></i></button>
-              </abbr>
+            <div class="row">
+                <div class="col-md-8">
+          <h2 class="card-title">Duendes</h2>
+                </div>
 
-            </form>
+                <div class="col-md-4">
+          <h2 class="font-weight-bold">0</h2>
+                </div>
+            </div>
 
+          <img src="https://usagif.com/wp-content/uploads/2022/hqgif/christmas-elves-21-dancing-elfs-tranpsarent-background.gif" class="img-fluid" alt="Duendes Navideños">
+          <button type="button" class="btn btn-primary btn-block">Agregar proceso de Duende</button>
+          </div>
+          </div>
+        </div>
+       
+        <div class="col-md-15">
+        <div class="card" style="margin: 50px;box-shadow: 0px 0px 10px gray">
+            <div class="card-body">
+            
+          <h2>Santa Claus Durmiendo</h2>
+       
+
+          <img src="https://i.gifer.com/origin/ac/ac6384a6e036b6d80d0c733fee379bd6.gif" class="img-fluid" alt="Santa Claus Dormido">
+          </div>
           </div>
 
-
-          <div class="col-lg-4 form-group text-center pb-2">
-
-            <form method="post" id="formulario">
-              <h4> <label for="new">Eliminar procesos</label></h4>
-
-              <input type="number" id="new" name="new" value="<?php echo $procesos ?>" min="0"
-                max="<?php echo $procesos ?>">
-
-              <abbr title="Elminar procesos">
-                <button type="submit" class="px-2 btn btn-danger"><i class="bi bi-trash3"></i></button>
-              </abbr>
-
-            </form>
-          </div>
-          <div class="col-lg-4 text-center">
-            <h5> Procesos corriendo <br> <strong>
-                <?php echo $procesos; ?>
-              </strong> </h5>
-
-          </div>
         </div>
       </div>
     </div>
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-      integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
-      crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
-      integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
-      crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
-      integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
-      crossorigin="anonymous"></script>
-</body>
-
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+  </body>
 </html>
+
